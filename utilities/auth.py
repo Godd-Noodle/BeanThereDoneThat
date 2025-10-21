828a404696842440f5c2d96e303993ef32fa0c32
Patrick McG <patrickdmcgurnaghan@gmail.com>
2025-10-21 22:08:50 +0100

    working wrappers for is_user and is_admin

--- utilities/auth.py
+++ utilities/auth.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timedelta
 from functools import wraps
 from typing import Any
+from bson import ObjectId
 from flask import request, jsonify
 import jwt
 from hashlib import pbkdf2_hmac
@@ -22,10 +24,6 @@ def create_collection_connection(collection_name: str, database_name : str = "BT
     return client.get_database(database_name).get_collection(collection_name)
 
 
-
-
-
-
 def generate_password_hash(password_string: str | None):
 
     if password_string is None:
@@ -34,49 +32,57 @@ def generate_password_hash(password_string: str | None):
     return pbkdf2_hmac('sha256', password_string.encode('ascii'), __salt, __iters)
 
 
-def create_token() -> jwt.JWT: pass
-
-
-
 
 def verify_user(func : callable):
     """
     A wrapper that only allows access to the function if the correct user permissions from a JWT are given.
 
-    kwargs added are user_id,user_name session_exp, is_admin, is_verified todo : add more terms as needed
+    kwargs added are user_id,name, email, session_exp, is_admin, is_verified
 
 
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print("verifing user")
-
-
 
         token = request.cookies.get('jwt')
-        jwt_values = {}
 
         if token is None:
             return jsonify({"message": "Token is missing"}), 401
 
-
-
+        # todo : remove finally expression when token creation and storage is successful
         try:
-             jwt_values = (jwt.decode(token, __dotenv_values["JWT_SECRET"]))
+            ...
+             #jwt_values = jwt.decode(token, __salt)
         except jwt.ExpiredSignatureError:
             return jsonify("Token is expired"), 401
         except jwt.InvalidTokenError:
             return jsonify("Token is invalid"), 401
-
-        # todo : verify is the user is valid by asking the db
-
+        finally:
+            jwt_values = {
+                "user_id": "68f277b7cff87a6e29e75555",
+                "exp": datetime.now() + timedelta(minutes=15)
+            }
 
         if "user_id" not in jwt_values:
-            return jsonify("Token is malformed, please login in again. If this problem persists. contact support"), 401
+            return jsonify("Token is malformed, please login in again. If this problem persists, then contact support"), 401
+
+        user_collection = create_collection_connection("Users")
+        this_user = user_collection.find_one({"_id" : ObjectId(jwt_values["user_id"])})
+
+        if this_user is None or this_user["is_deleted"]:
+            return jsonify("User not found from JWT. Perhaps the account has been deleted"), 401
 
 
-        # todo : add to the kwargs
+        if not this_user["verified"]:
+            return jsonify("User has not verified their account yet"), 401
 
+
+        kwargs["user_id"] = jwt_values["user_id"]
+        kwargs["name"] = this_user["name"]
+        kwargs["user_email"] = this_user["email"]
+        kwargs["session_exp"] = jwt_values["exp"]
+        kwargs["is_admin"] = this_user["is_admin"]
+        kwargs["is_verified"] = this_user["verified"]
 
         result= func(*args, **kwargs)
 
@@ -89,22 +95,16 @@ def verify_user(func : callable):
     return wrapper
 
 
-
-def verify_admin(jwt):
+@verify_user
+def verify_admin(func : callable):
     """
     A wrapper that only allows access to the function if the user is an admin.
-
-
-
-
+    kwargs added are user_id,name, email, session_exp, is_admin, is_verified
     """
-    # todo : make this function a wrapper with check to see if kwargs[admin] is true
+    @wraps(func)
+    def wrapper(*args, **kwargs):
 
-    ...
+        if not kwargs["is_admin"]:
+            return jsonify("User is not admin"), 401
 
-
-
-
-
-
-
+        return func(*args, **kwargs)
