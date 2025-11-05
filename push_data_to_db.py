8c860eeec4368e74b3319686e120a98dbb14fad5
Patrick McG <patrickdmcgurnaghan@gmail.com>
2025-11-05 15:22:44 +0000

    adds sample reviews to each shop

--- push_data_to_db.py
+++ push_data_to_db.py
@@ -1,33 +1,64 @@
 import json
 from datetime import datetime
+from logging import exception
+
 from dateutil import tz
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 import dotenv
 from faker import Faker
-from hashlib import pbkdf2_hmac
 from time import sleep
 import urllib.request
 import urllib.parse
 
+from utilities import auth
+
+import random
 
 
 env_values = dotenv.dotenv_values()
-uri = env_values['MONGO_URI']
-salt = env_values['SALT'].encode('ascii')
-iters = int(env_values['ITERS'])
 geo_api_key = env_values['GEO_API_KEY']
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+list_of_valid_stores = [
+    "Coffee shop",
+    "Cafe",
+    "Convenience store",
+    "Bakery",
+    "Restaurant",
+    "Supermarket",
+    "Grocery store",
+    "Furniture store",
+    "Sandwich shop",
+    "Bar & grill",
+    "Ice cream shop",
+    "Fast food restaurant",
+    "Fish and chips takeaway",
+    "Deli",
+    "Coffee roasters",
+    "Dessert shop",
+    "Italian restaurant",
+    "Pub",
+    "Pizza restaurant",
+    "Irish restaurant",
+    "Family restaurant",
+    "Bistro",
+    "Convenience Store",
+    "Breakfast restaurant",
+    "Department store",
+    "Book store",
+    "Coffee machine supplier",
+    "Kebab shop",
+    "Antique store",
+    "Modern European restaurant"]
 
 
-def create_collection_connection(collection_name: str):
-    return client.get_database("BTDT").get_collection(collection_name)
+fake = Faker()
 
+user_collection = auth.create_collection_connection("Users")
+shop_collection = auth.create_collection_connection("Shops")
+
+
+users : dict = None
 
 def print_first_ten_geo():
-    shop_collection = create_collection_connection("Shops")
     shop_collection.create_index([("location", "2dsphere")])
 
 
@@ -62,9 +93,9 @@ def print_first_ten_geo():
 
 
 def main():
-    create_users(10)
+    create_users(20)
 
-    #populate_businesses()
+    populate_shops()
 
     #add_long_lat()
 
@@ -82,61 +113,90 @@ def create_users(user_count : int = 50):
 
 
 
-    fake = Faker()
+
 
     data = [
         {
         "name" : "Patrick McGurnaghan",
         "email" : "mcgurnaghan-p1@ulster.ac.uk",
         "dob": datetime(2000, 6,23, tzinfo=tz.tzutc()).replace(microsecond=0,tzinfo=tz.tzutc()),
-        "is_admin": 1,
-        "is_deleted": 0,
-        "verified": 1,
-        "password": passwordify("password"),
+        "admin": True,
+        "deleted": False,
+        "verified": True,
+        "password": auth.generate_password_hash("password"),
         "sessions" : []
         }
     ]
 
 
-    for i in range(10):
+    for i in range(user_count):
         name = fake.name()
         person = {
         "name" : f"{name}{i}",
         "email" : name.replace(" ",".").lower() + "@fake.com",
         "dob": fake.date_time_between(datetime(1970,1,1),datetime(2000,1,1),tz.tzutc())
             .replace(hour=0,minute=0,second=0,microsecond=0,tzinfo=tz.tzutc()),
-        "is_admin": 0,
-        "is_deleted": 0,
-        "password": passwordify(f"password{i}"),
+        "admin": False,
+        "deleted": False,
+        "password": auth.generate_password_hash(f"password{i}"),
         "sessions": [],
-        "verified": 1
+        "verified": True
         }
         data.append(person)
 
-    #push data to users table in mongodb
-    user_collection = create_collection_connection("Users")
 
     #delete all in collection
     user_collection.drop()
 
 
-    user_collection.insert_many(data)
+    result = user_collection.insert_many(data)
+
+    [str(_id) for _id in result.inserted_ids]
 
     print("Users created")
 
 
-def populate_businesses():
+def populate_shops():
     import json
 
     with open('places_2025-10-16.json') as f:
         d = json.load(f)
 
 
+    shops = [shop for shop in d if shop.get('categoryName') and shop.get('categoryName') in list_of_valid_stores]
 
-    # push data to users table in mongodb
-    shop_collection = create_collection_connection("Shops")
+    for shop in shops:
+        if shop.get('categoryName'):
+            shop["type"] = shop['categoryName']
+            del shop['categoryName']
+        if shop.get('url'):
+            del shop["url"]
+
+    reviews_each = 10
+    comments_each = 10
+
+
 
     shop_collection.drop()
+
+
+    comment_blueprint= {
+    "user_id" : None,
+    "message" : None,
+    "time_created" : None,
+    "edits": [],
+    "likes" : [],
+    "deleted" : False
+    }
+
+
+    for shop in shops:
+        shop["deleted"] = False
+        shop["reviews"] = make_reviews(length=reviews_each)
+        shop["photo"] = None
+
+
+
     shop_collection.insert_many(d)
 
 
@@ -145,7 +205,6 @@ def populate_businesses():
 
 
 def add_long_lat():
-    shop_collection = create_collection_connection("Shops")
     while True:
         shop = shop_collection.find_one({"location": None})
 
@@ -194,21 +253,47 @@ def add_long_lat():
     print("lat and long added to shops")
 
 
+def make_reviews(length : int = 10, include_comments:bool=False) -> list[dict]:
+
+    user_ids = [str(doc['_id']) for doc in user_collection.aggregate([
+                {'$sample': {'size': length}}
+                ])]
+
+    #print(len(user_ids))
+    review_list : list[dict] = []
 
 
 
+    for i in range(length):
+
+        dt= fake.date_time_between(datetime(2020,1,1),datetime(2025,11,1),tz.tzutc())
 
 
+        review_list.append({
+        "user_id": user_ids[i],
+        "message": fake.sentence(nb_words=15),
+        "score": random.randint(1,5),
+        "date_created": dt,
+        "date_edited": dt,
+        "edits": [],
+        "comments": make_comments() if include_comments else [],
+        "likes": {},
+        "deleted": False,
+        "photo": None,
+        })
 
-def passwordify(psswrd: str):
-    return pbkdf2_hmac('sha256', psswrd.encode('ascii'), salt, iters)
 
+    return review_list
+
+
+def make_comments(length : int = 10) -> dict:
+    raise exception("not implemented")
 
 
 if __name__ == '__main__':
 
-    i = input("type 'y' to migrate db")
-
-    if i == "y":
+    # length = input("type 'y' to migrate db")
+    #
+    # if length == "y":
 
         main()
